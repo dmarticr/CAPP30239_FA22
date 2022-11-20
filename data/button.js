503d5876993082ea98507f9015313f777f0cb94b
Diego Alejandro Martínez Cruz <dmarticr@uchicago.edu>
2022-11-20 14:51:12 -0600

    final project charts

---
+++ data/button.js
@@ -0,0 +1,109 @@
+const data1 = [
+	{group: 'Action', value: 54.09},
+	{group: 'Action-Adventure', value: 230.18},
+	{group: 'Adventure', value: 140.59},
+	{group: 'Education', value: 0.09},
+	{group: 'Fighting', value: 158.15},
+	{group: 'Misc', value: 240.31},
+	{group: 'Music', value: 13.64},
+	{group: 'Party', value: 171.75},
+	{group: 'Platform', value: 1106.15},
+	{group: 'Puzzle', value: 661.05},
+	{group: 'Racing', value: 387.49},
+	{group: 'Role-Playing', value: 923.43},
+	{group: 'Sandbox', value: 2.2},
+	{group: 'Shooter', value: 118.6},
+	{group: 'Simulation', value: 259.08},
+	{group: 'Sports', value: 405.29},
+	{group: 'Strategy', value: 51.06}
+];
+ 
+const data2 = [
+	{group: 'North America', value: 3377.38},
+	{group: 'Europe', value: 1934.83},
+	{group: 'Japan', value: 686.1500000000001},
+	{group: 'Other', value: 657.7800000000001},
+	{group: 'Unclassified', value: 19034.140000000003}
+];
+ 
+// set the dimensions and margins of the graph
+const margin = {top: 30, right: 30, bottom: 120, left: 65},
+	width = 460 - margin.left - margin.right,
+	height = 460 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#button_chart")
+	.append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", `translate(${margin.left},${margin.top})`)
+	.attr('font-weight','bold');
+
+svg.append("text")
+	.attr("class", "y-label")
+	.attr("text-anchor", "end")
+	.attr("x", margin.top - 15)
+	.attr("dx", "-0.5em")
+	.attr("y", -55)
+	.attr("transform", "rotate(-90)")
+	.style("font-size","15px")
+	.text("US Millions")
+	.style('fill', 'purple');
+	
+ // Initialize the X axis
+ const x = d3.scaleBand()
+	.range([ 0, width ])
+	.padding(0.1);
+
+ const xAxis = svg.append("g")
+  	.attr("transform", `translate(0,${height + 1})`);
+
+ 
+ // Initialize the Y axis
+ const y = d3.scaleLinear()
+   	.range([ height, 0]);
+
+ const yAxis = svg.append("g")
+   .attr("class", "myYaxis")
+ 
+ // A function that create / update the plot for a given variable:
+ function update(data) {
+ 
+   	// Update the X axis
+   	x.domain(data.map(d => d.group))
+
+   	xAxis.call(d3.axisBottom(x))
+		.selectAll("text")  
+		.style("text-anchor", "end")
+		.attr("dx", "-.8em")
+		.attr("dy", ".15em")
+		.attr("transform", "rotate(-65)")
+		.style("font-size","12px");
+ 
+	// Update the Y axis
+	y.domain([0, d3.max(data, d => d.value) ]);
+
+   	yAxis.transition()
+   		.duration(1000)
+		.call(d3.axisLeft(y).tickFormat(d => "$" + d))
+		.attr('font-weight','bold')
+		.selectAll("text")
+		.style("font-size", "12px");
+ 
+	// Create the u variable
+	var u = svg.selectAll("rect")
+	 	.data(data)
+ 
+   	u.join("rect") // Add a new rect for each new elements
+		.transition()
+		.duration(1000)
+		.attr("x", d => x(d.group))
+		.attr("y", d => y(d.value))
+		.attr("width", x.bandwidth())
+		.attr("height", d => height - y(d.value))
+		.attr("fill", "#fb9a99");
+ }
+
+ // Initialize the plot with the first dataset
+ update(data1)
---
+++ data/stackedbar.js
@@ -0,0 +1,165 @@
+d3.csv("g11.csv").then( function(data) {
+
+	// set the dimensions and margins of the graph
+	const margin = {top: 10, right: 30, bottom: 35, left: 70},
+	width =  600 - margin.left - margin.right,
+	height = 400 - margin.top - margin.bottom;
+
+	// append the svg object to the body of the page
+	const svg = d3.select("#stackedbar")
+		.append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", `translate(${margin.left},${margin.top})`);
+
+	// List of subgroups = header of the csv files
+	const subgroups = data.columns.slice(1)
+
+	// List of groups = value of the first column called group -> I show them on the X axis
+	const groups = data.map(d => d.Year)
+
+	// Add X axis
+	const x = d3.scaleBand()
+				.domain(groups)
+				.range([0, 300])
+				.padding([0.2])
+	
+	svg.append("g")
+		.attr("transform", `translate(0, ${height + 1})`)
+		.attr("class", "x-axis")
+		.attr('font-weight','bold')
+		.call(d3.axisBottom(x)
+				.tickValues(x.domain()
+							 .filter(function(d, i){ return !(i % 10)})
+							)
+			)
+		.selectAll("text")
+		.style("font-size","12px");
+
+	svg.append("text")                  
+		.attr("class", "x-label")
+		.attr("text-anchor", "end")
+		.attr("x", width - 210)
+		.attr("y", height + 40)
+		.attr("dx", "0.5em")
+		.attr("dy", "-0.5em")
+		.style("font-size", "15px")
+		.text("Year")
+		.style('fill', 'purple');
+
+	// Add Y axis
+	const y = d3.scaleLinear()
+              	.domain([0, 1600])
+             	.range([height, 0 ]);
+
+  	svg.append("g")
+		.call(d3.axisLeft(y).tickFormat(d => "$" + d))
+		.attr('font-weight','bold')
+		.selectAll("text")
+		.style("font-size", "12px");
+
+	svg.append("text")
+		.attr("class", "y-label")
+		.attr("text-anchor", "end")
+		.attr("x", margin.top)
+		.attr("dx", "-0.5em")
+		.attr("y", -50)
+		.attr("transform", "rotate(-90)")
+		.style("font-size","15px")
+		.text("US Millions")
+		.style('fill', 'purple');
+
+	// color palette = one color per subgroup
+	const color = d3.scaleOrdinal()
+                  	.domain(subgroups)
+                 	.range(d3.schemeSet3);
+					//.range(["#bf5b17", "#7fc97f","#f0027f","#ccc","#666666","#386cb0","#fdc086","#beaed4"]);
+
+	//stack the data? --> stack per subgroup
+	const stackedData = d3.stack().keys(subgroups)(data)
+
+ svg.selectAll("mydots")
+	.data(subgroups)
+	.enter()
+	.append("circle")
+	.attr("cx", 325)
+	.attr("cy", function(d,i){ return 10 + i * 25}) // 100 is where the first dot appears. 25 is the distance between dots
+	.attr("r", 5)
+	.style("fill", function(d){ return color(d)})
+
+// Add one dot in the legend for each name.
+svg.selectAll("mylabels")
+	.data(subgroups)
+	.enter()
+	.append("text")
+	.attr("x", 340)
+	.attr("y", function(d,i){ return 10 + i * 25}) // 100 is where the first dot appears. 25 is the distance between dots
+	.style("fill", function(d){ return color(d)})
+	.text(function(d){ return d})
+	.attr("text-anchor", "left")
+	.style("alignment-baseline", "middle")
+	.attr('font-weight','bold')
+	.style("font-size","15px")
+	.on("mouseover", function (event,d) { // What happens when user hover a bar
+
+          // what subgroup are we hovering?
+          const subGroupName = d3.select(this.parentNode).datum().key
+
+          // Reduce opacity of all rect to 0.2
+          d3.selectAll(".myRect").style("opacity", 0.2)
+
+          // Highlight all rects of this subgroup with opacity 1. It is possible to select them since they have a specific class = their name.
+          d3.selectAll("." + subGroupName).style("opacity",1) 
+
+      })
+      .on("mouseleave", function (event,d) { // When user do not hover anymore
+
+          // Back to normal opacity: 1
+          d3.selectAll(".myRect")
+            .style("opacity",1)
+      });
+
+  // ----------------
+  // Highlight a specific subgroup when hovered
+  // ----------------
+
+  // Show the bars
+  svg.append("g")
+      .selectAll("g")
+
+      // Enter in the stack data = loop key per key = group per group
+      .data(stackedData)
+      .join("g")
+      .attr("fill", d => color(d.key))
+      .attr("class", d => "myRect " + d.key ) // Add a class to each subgroup: their name
+      .selectAll("rect")
+
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(d => d)
+      .join("rect")
+      .attr("x", d => x(d.data.Year))
+      .attr("y", d => y(d[1]))
+      .attr("height", d => y(d[0]) - y(d[1]))
+      .attr("width", x.bandwidth())
+      .attr("stroke", "white")
+      .on("mouseover", function (event,d) { // What happens when user hover a bar
+
+          // what subgroup are we hovering?
+          const subGroupName = d3.select(this.parentNode).datum().key
+
+          // Reduce opacity of all rect to 0.2
+          d3.selectAll(".myRect").style("opacity", 0.2)
+
+          // Highlight all rects of this subgroup with opacity 1. It is possible to select them since they have a specific class = their name.
+          d3.selectAll("." + subGroupName).style("opacity",1) 
+ 
+
+      })
+      .on("mouseleave", function (event,d) { // When user do not hover anymore
+
+          // Back to normal opacity: 1
+          d3.selectAll(".myRect")
+            .style("opacity",1)
+      })
+})
